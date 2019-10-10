d98ab1ea9a785dd63647c1550bbc7d4adefce3e7
Lichen Ma <lichenma123@gmail.com>
2019-10-10 19:48:35 -0400

    frontend form submission and validations working

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,8 @@
 class Image < ApplicationRecord
     has_one_attached :picture
+
+    # TODO validate the presence of image 
+    # validations - ensures that title and created_by are provided and that visibility must be set to either public or private
+     validates_presence_of :title, :created_by, :picture
+     validates :visibility, :inclusion => { :in => %w(public private), :message => "%{value} must be public or private" }
 end
